4aecddd80f07a294671654e3a27d1145493151d6
Christopher Ayre <christopher.develop@hotmail.com>
2025-04-29 17:53:48 +0800

    feat: List changed files committed.
    Get the list of files that are changed that have been committed. We don't
    count staged or un-staged files here as these would not count towards a
    push witch is probably the most likely hook to use this function.

--- git_hooks.go
+++ git_hooks.go
@@ -3,14 +3,24 @@ package githooks
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
-func GetChangedFiles(targetBranchName string) {
+/*
+* GetChangedFiles retrieves the list of files changed in the current branch compared to the target branch.
+* Note these are only files that have been marked as changed by git, i.e. Committed files.
+* @param targetBranchName: The name of the target branch to compare against.
+* @return: A slice of strings containing the names of the changed files.
+* @error: An error if the git command fails to execute.
+ */
+func GetChangedFiles(targetBranchName string) ([]string, error) {
 	cmd := exec.Command("git", "diff", "--name-only", targetBranchName)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+		return nil, err
 	}
-	fmt.Println(string(output))
+
+	splitStrings := strings.Split(string(output), "\n")
+	return splitStrings, nil
 }
--- git_hooks_test.go
+++ git_hooks_test.go
@@ -1,9 +1,18 @@
 package githooks
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestStatus(t *testing.T){
-	GetChangedFiles("main");
+func TestPrintGetChangedFiles(t *testing.T){
+	fmt.Println("Files changed on main:")
+	files, err := GetChangedFiles("main")
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	for _, element := range files{
+		fmt.Println("\t" + element)
+	}
 }
