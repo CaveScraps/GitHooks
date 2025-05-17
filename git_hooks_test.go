0349d1f6cb0354a495bf0048daab030a9c3bb0f9
Christopher Ayre <christopher.develop@hotmail.com>
2025-05-17 19:07:49 +0800

    test: GetChangedFiles test
    Adds testing for the GetChangedFiles function. More importantly we have
    found a way to test all these functions. This proved difficult because
    it relies on testing git history but we dont want to effect the repos
    history.
    
    What we have ended up doing is create a git submodule directory, testing in
    there and clean it up after we are done.

--- git_hooks_test.go
+++ git_hooks_test.go
@@ -3,9 +3,31 @@ package githooks
 import (
 	"fmt"
 	"testing"
+	"os/exec"
 )
 
-func TestPrintGetChangedFiles(t *testing.T){
+const (
+	testFolder = "test_folder"
+)
+
+var (
+	testFiles  = []string{"test_file.txt", "test_file2.txt"}
+)
+
+
+func TestGetChangedFiles(t *testing.T){
+	// Stash any current changes to avoid conflicts
+	exec.Command("git", "stash").Run()
+	// Create a folder and a file to test the function
+	exec.Command("mkdir", "test_folder").Run()
+	exec.Command("touch", "test_folder/test_file.txt").Run()
+	exec.Command("cd", "test_folder").Run()
+	// Start a git repository
+	exec.Command("git", "init").Run()
+	exec.Command("git", "add", "test_folder/test_file.txt").Run()
+	exec.Command("git", "commit", "-m", "test commit").Run()
+
+	// Call the function to get changed files
 	fmt.Println("Files changed on main:")
 	files, err := GetChangedFiles("main")
 	if err != nil{
@@ -15,4 +37,8 @@ func TestPrintGetChangedFiles(t *testing.T){
 	for _, element := range files{
 		fmt.Println("\t" + element)
 	}
+	// Clean up
+	exec.Command("cd", "-").Run()
+	exec.Command("rm", "-rf", "test_folder").Run()
+	exec.Command("git", "stash", "pop").Run()
 }
